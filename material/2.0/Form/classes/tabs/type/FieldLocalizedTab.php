614eb0cb9830d489f9651c8acd2867495e31ebd4
Vitaly Iegorov <egorov@samsonos.com>
2014-06-06 14:51:15 +0300

    Fixed bug with empty locale sub tabs

--- material/2.0/Form/classes/tabs/type/FieldLocalizedTab.php
+++ material/2.0/Form/classes/tabs/type/FieldLocalizedTab.php
@@ -42,6 +42,7 @@ class FieldLocalizedTab extends FormTab
             // If it is not empty
             if($tab->filled()) {
                 $this->tabs[] = $tab;
+                trace('Adding tab '.$locale);
             }
 		}		
 	}	
