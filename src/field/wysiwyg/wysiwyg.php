b6c62d37c75b90154b609cd6934a6305f1f4b413
Vitaly Iegorov <egorov@samsonos.com>
2014-08-22 18:19:04 +0300

    Fixed main icons

--- src/field/wysiwyg/wysiwyg.php
+++ src/field/wysiwyg/wysiwyg.php
@@ -6,12 +6,10 @@ namespace samson\cms\input;
  * @author Vitaly Iegorov<egorov@samsonos.com>
  */
 class Wysiwyg extends Field 
-{		
+{
+    /** @var string Module identifier */
+    public $id = 'samson_cms_input_wysiwyg';
+
 	/** Special CSS classname for nested field objects to bind JS and CSS */
 	protected $cssclass = '__wysiwyg';
-
-    /** Module dependencies */
-    protected $requirements = array(
-        //'ckeditor'
-    );
 }
