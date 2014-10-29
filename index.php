abe3bcbeee508e122a63debf7369336e55440a60
Vitaly Iegorov <vitalyiegorov@gmail.com>
2014-10-29 12:47:05 +0200

    Added automatic correct base path setting independently of real cms web-application location

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ define('__PATH', '');
 /** Set new project structure vendor path */
 define('__SAMSON_VENDOR_PATH', __PATH.'vendor/');
 
+/** Set correct relative base path */
+define('__SAMSON_BASE__', '/'.basename(__DIR__).'/');
+
 /** Load composer autoloader */
 require(__SAMSON_VENDOR_PATH.'autoload.php');
 
