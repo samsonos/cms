a8fecd6dde250f31673f8eab24ff5b966a08de50
Vitaly Egorov <egorov@samsonos.com>
2014-11-07 12:41:57 +0200

    Changed project srtucture Changed init script

--- app/config/ActiveRecordTestConfig.php
+++ app/config/ActiveRecordTestConfig.php
@@ -4,8 +4,8 @@
  * on 19.08.14 at 16:05
  */
 
-/** Конфигурация DEV для ActiveRecord */
-class ActiveRecordConfig extends \samson\core\Config
+/** Test ActiveRecord configuration for development */
+class ActiveRecordTestConfig extends \samson\core\Config
 {
     public $__module = 'activerecord';
 
