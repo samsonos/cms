ede54862d0dd181c4e69325f24a476b236d7ec87
Vitaly Egorov <egorov@samsonos.com>
2014-11-07 15:33:57 +0200

    Changed project srtucture Changed init script

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
-/** Set new project structure vendor path */
-define('VENDOR_PATH', __PATH.'vendor/');
-
 /** Set correct relative base path */
 define('__SAMSON_BASE__', '/'.basename(__DIR__).'/');
 
 /** Load composer autoloader */
-require(VENDOR_PATH.'autoload.php');
+require('vendor/autoload.php');
 
 /** Configuration section */
 
 /** Load SamsonCMS **/
-require(__PATH.'www/index.php');
+require('www/index.php');
